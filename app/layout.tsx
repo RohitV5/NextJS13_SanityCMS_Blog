37098ff3434cca2b52deed3433cf101750609d54
Rohit Verma <verma.rohit.in@gmail.com>
2023-01-15 16:33:12 +0530

    Chore : Project init

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+import "../styles/global.css"
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  )
+}
