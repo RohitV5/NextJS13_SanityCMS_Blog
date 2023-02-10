929c8c4d55c66b4e016e34c02d03b17246ef5efe
Rohit Verma <verma.rohit.in@gmail.com>
2023-02-11 00:31:22 +0530

    feature: Refresh SSG pages every 60 seconds

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -12,6 +12,8 @@ type Props = {
   };
 };
 
+export const revalidate = 60;  //revalidates this page every 60 seconds in SSG
+
 export async function generateStaticParams() {
   const query = groq` *[_type=='post']
   {
